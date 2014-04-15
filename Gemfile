4d4be0359f164d02b1c25480276977afdf381f32
Mariano <mjmateuci@gmail.com>
2014-04-15 02:54:20 -0300

    prepare to heroku

--- Gemfile
+++ Gemfile
@@ -6,7 +6,7 @@ gem 'therubyracer'
 gem 'rails', '4.1.0'
 gem 'protected_attributes'
 # Use sqlite3 as the database for Active Record
-gem 'sqlite3'
+gem 'pg'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 4.0.3'
 # Use Uglifier as compressor for JavaScript assets
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140414164329) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "catalogs", force: true do |t|
     t.string   "name"
     t.string   "description"
